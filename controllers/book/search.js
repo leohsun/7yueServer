2c3164f4a7f72366df5e5c663e0cbcedd89a9690
leohsun <leohsun@qq.com>
2018-09-13 13:54:54 +0800

    change search type  title is specified

--- controllers/book/search.js
+++ controllers/book/search.js
@@ -4,14 +4,14 @@ module.exports = {
     description: 'book search list api',
     handler: async function (request, h) {
         let { page, size, keyword } = request.payload;
-        if(!page) page = 1;
-        if(!size) size = 2;
+        if (!page) page = 1;
+        if (!size) size = 2;
         const skip = (page - 1) * size;
         console.log('keyword: ', request.payload);
-        if(!keyword) return h.success({data:[],page,size,hasMore:false});
+        if (!keyword) return h.success({ data: [], page, size, hasMore: false });
         const Book = mongoose.model('Book')
-        const hot_book_list = await Book.find({ '$or': [{ title: {'$regex': keyword} }, { author: {'$regex': keyword} }, { summary: {'$regex': keyword} }] }).skip(skip).limit(size);
-        const total = await Book.find({ '$or': [{ title: {'$regex': keyword} }, { author: {'$regex': keyword} }, { summary: {'$regex': keyword} }] }).countDocuments();
+        const hot_book_list = await Book.find({ title: { '$regex': keyword } }).skip(skip).limit(size);
+        const total = await Book.find({ '$or': [{ title: { '$regex': keyword } }, { author: { '$regex': keyword } }, { summary: { '$regex': keyword } }] }).countDocuments();
         const hasMore = skip + size < total
         const data = {
             page,
