594a9c20c9dbe62935d80b3b70a3b6c2e3fef539
leohsun <leohsun@qq.com>
2018-09-16 23:31:44 +0800

    add image key to periodical response data

--- controllers/periodical/detail.js
+++ controllers/periodical/detail.js
@@ -7,7 +7,7 @@ module.exports = {
         const idx = +request.url.query.index || 'latest'
         const Periodical = mongoose.model('Periodical');
         const periodical_list = idx !== 'latest' ? await Periodical.findOne({ index: idx }) : await Periodical.find().sort({ index: -1 });
-        const { type, content, index, title, _id } = periodical_list[0];
+        const { type, content, index, title,image } = periodical_list[0];
         const preIdx = index - 1;
         const nextIdx = index + 1;
         const previours = await Periodical.findOne({index: preIdx});
@@ -16,7 +16,7 @@ module.exports = {
         const fav_nums = await Like.find({ periodicalId: _id }).countDocuments();
         const like_status = await Like.find({ '$and': [{ periodicalId: _id }, { userId }] }).countDocuments();
         const data = {
-            data :{ type, content, index, title, fav_nums, like_status, previours, next }
+            data :{ type, content, index, title,image, fav_nums, like_status, previours, next }
         }
 
 
